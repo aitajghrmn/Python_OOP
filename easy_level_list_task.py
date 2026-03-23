5c24aa6cf6c151aa43402845bf58f423c2a91ac2
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-03-23 16:39:45 +0400

    Add files via upload

--- easy_level_list_task.py
+++ easy_level_list_task.py
@@ -1,7 +1,7 @@
 numbers=[1,2,3,4,5,6,7,8,9,10]
-tek=[]
+cut=[]
 for num in numbers:
-    if num % 2 !=0:
-        tek.append(num)
+    if num % 2 == 0:
+        cut.append(num)
 
-print(tek)
+print(cut)
