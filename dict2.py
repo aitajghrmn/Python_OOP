2a001025a07febb23453e84096000b3d3704c4c8
aitajghrmn <businessqehremanqizi@gmail.com>
2026-03-23 20:31:19 +0400

    dictionary task

---
+++ dict2.py
@@ -0,0 +1,11 @@
+name = input("Ad daxil et: ")
+age = int(input("Yas daxil et: "))
+grade = int(input("Qiymet daxil et: "))
+
+student = {
+    "name": name,
+    "age": age,
+    "grade": grade
+}
+
+print(student)
