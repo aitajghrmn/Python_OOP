fc8a8d5bd82be7073e52d01587dbd7f5a3cabe66
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-02 13:42:29 +0400

    inheritence

--- class_inheritence.py
+++ class_inheritence.py
@@ -9,8 +9,15 @@ class ElectricCar(Car):
         super().__init__(brand, year)
         self.battery = battery 
 
+
+
+    def describe(self):
+        return (self.brand + " " + "is an electric car with " + " " + str(self.battery))
+    
+
 car1=ElectricCar("Tesla",2020,100)
 print(car1.brand)
 print(car1.battery)
+print(car1.describe())
 
 
