2971294114c3e7f8012bdac5920db49eed260ebb
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-02 13:34:46 +0400

    Task for inheritence

---
+++ class_inheritence.py
@@ -0,0 +1,16 @@
+class Car:
+    def __init__(self, brand, year):
+        self.brand = brand
+        self.year = year
+
+
+class ElectricCar(Car):
+    def __init__(self, brand, year, battery):
+        super().__init__(brand, year)
+        self.battery = battery 
+
+car1=ElectricCar("Tesla",2020,100)
+print(car1.brand)
+print(car1.battery)
+
+
