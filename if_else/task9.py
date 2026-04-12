72b36d6d2a2952a1c8fa922f36b5efae3bfc87f0
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-12 23:46:33 +0400

    Add season determination based on month input

---
+++ if_else/task9.py
@@ -0,0 +1,10 @@
+ay = int(input("Ay nomresini daxil et: "))
+
+if ay == 12 or ay == 1 or ay == 2:
+    print("Qis")
+elif ay == 3 or ay == 4 or ay == 5:
+    print("Yaz")
+elif ay == 6 or ay == 7 or ay == 8:
+    print("Yay")
+else:
+    print("Payiz")
