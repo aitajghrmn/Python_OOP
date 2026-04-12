1b3463e3a0e2b587e41fb5ba44a29564e5b035a7
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-12 23:45:42 +0400

    Add day of the week selection based on input number

---
+++ if_else/task8.py
@@ -0,0 +1,10 @@
+gun = int(input("Heftenin gun nomresini daxil et: "))
+
+if gun == 1:
+    print("Bazar ertesi")
+elif gun == 2:
+    print("Cersenbe axsami")
+elif gun == 3:
+    print("Cersenbe")
+else:
+    print("Diger gun")
