a2690e48ab399c7b786a0963f78b13649b8af088
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-14 13:57:42 +0400

    Add error handling for division with finally clause
    Implement try-except-finally structure for division.

---
+++ exceptions/finally.py
@@ -0,0 +1,15 @@
+try :
+    a=int(input("Enter a number: "))
+    b=int(input("Enter a number: "))
+
+    print(a/b)
+
+except ValueError:
+    print("Please enter a valid number.")
+
+
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+
+finally:
+    print("Everything is done.")
