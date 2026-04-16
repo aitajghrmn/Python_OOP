022d5a8c77d83d0e68f4b6159164370a6b5093f5
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-16 10:22:10 +0400

    Add user list and print names in task1.py

---
+++ dict/dict-with-list/task1.py
@@ -0,0 +1,10 @@
+users=[
+    {"name" : "Aitaj" , "age" : 21 },
+    {"name" : "Tural" , "age" : 23 },
+    {"name" : "Nigar" , "age" : 24 },
+    {"name" : "Murad" , "age" : 22},
+    {"name" : "Ali" , "age" :25 }
+    ]
+    
+for user in users:
+    print(user["name"])
