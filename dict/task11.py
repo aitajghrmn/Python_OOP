e560f357af8acae1d81932e9c8e506868fca8cc5
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-19 11:48:27 +0400

    Create task11.py to list users older than 20
    Add a script to filter users by age greater than 20

---
+++ dict/task11.py
@@ -0,0 +1,15 @@
+users = [
+    {"name": "Ali", "age": 20},
+    {"name": "Veli", "age": 25},
+    {"name": "Aytac", "age": 21}
+]
+
+
+
+age20 = []
+
+for u in users:
+    if u["age"]>20:
+        age20.append(u["name"])
+
+print(age20)
