b7b2b9f170cd3c955ff26deef1caa9d8bbfc1d26
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-19 11:42:58 +0400

    Add task9.py to print age of user 'Ali'

---
+++ dict/task9.py
@@ -0,0 +1,11 @@
+users = [
+    {"name": "Ali", "age": 20},
+    {"name": "Veli", "age": 25},
+    {"name": "Aytac", "age": 21}
+]
+
+
+
+for u in users:
+    if u["name"] =="Ali" :
+        print(u["age"])
