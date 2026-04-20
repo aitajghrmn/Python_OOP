89193cf1ef56a422a66a3f7056d1390e92175c47
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-20 14:41:20 +0400

    Add User class with example instances and prints

---
+++ classes/task1.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, name, age):
+        self.name= name
+        self.age = age
+
+user1=User("Aitaj" , 21)
+user2=User("Murad" , 24)
+
+print(user1.name)
+print(user2.name)
