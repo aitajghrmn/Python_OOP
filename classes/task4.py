c96f8e2d3e54a23109ccc9ec1b92bc48142204f8
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-04-20 15:27:47 +0400

    Refactor User class and age check logic

--- classes/task4.py
+++ classes/task4.py
@@ -1,20 +1,20 @@
 class User:
-    def __init__ (self,name,age):
-        self.name=name
-        self.age=age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-user1=User("Aitaj" , 21 )
-user2=User("Tural" , 23)
 
-if user1.age>18:
-    print("You are an adult")
+user1 = User("Aitaj", 21)
+user2 = User("Tural", 23)
 
+
+if user1.age >= 18:
+    print(user1.name, "is Adult")
 else:
-    print("You are a child")
+    print(user1.name, "is Child")
 
 
-if user2.age>18:
-    print("You are an adult")
-
+if user2.age >= 18:
+    print(user2.name, "is Adult")
 else:
-    print("You are a child")
+    print(user2.name, "is Child")
