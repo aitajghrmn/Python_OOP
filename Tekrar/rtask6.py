3787b95f6299685bcd0f6e51c18b5dda093934fd
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-05-15 10:42:59 +0400

    Add student management functionality in rtask6.py

---
+++ Tekrar/rtask6.py
@@ -0,0 +1,16 @@
+students = ["Ali", "Aytac", "Leyla", "Murad"]
+
+
+students.append("Nigar")
+
+
+students.remove("Ali")
+
+
+print("Tələbələr:")
+for student in students:
+    print(student)
+
+
+if "Aytac" in students:
+    print("Aytac siyahıdadır")
