767ecc1f11ffcddc6f52a2d2ef3e1b4509125fbe
Aitaj <businessqehremanqizi@gmail.com>
2026-05-29 19:08:14 +0400

    tekrar

---
+++ Tekrar/may/tapsirig1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+temp=int(input("Enter the themperature: "))
+
+if temp>30:
+   print("It is hot")
+elif temp>=15 and temp<=30 :
+   print("It is normal")
+else:
+   print("it is cold ")
