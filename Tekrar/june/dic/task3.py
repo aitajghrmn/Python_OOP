0bc40187cab7299c98528f74e298ce0237ec23e5
aitajghrmn <businessqehremanqizi@gmail.com>
2026-06-08 14:15:31 +0400

    task3 for dics

---
+++ Tekrar/june/dic/task3.py
@@ -0,0 +1,4 @@
+ballar = {"Aytac" : 95, "Emin" : 45 , "Gunel" : 70 , "Kamran" : 30}
+
+for telebe,qiymet in ballar.items():
+    print(f' {telebe["ad"]} adli telebenin qiymeti : {telebe["qiymet"]}')
