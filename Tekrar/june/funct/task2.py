c3412e4965ee2c2af1fb6746380bd22b4bf40857
Aitaj <businessqehremanqizi@gmail.com>
2026-06-13 17:58:33 +0400

    task 2 for functions

---
+++ Tekrar/june/funct/task2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''Find the maximum'''
+
+def find_maximum(num1, num2):
+    if num1 > num2 :
+        return num1
+    else:
+        return num2
+    
+print(find_maximum(23,11))
