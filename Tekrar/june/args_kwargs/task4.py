fbf5cbc3a8e58b558415aa2b62975cb08ce8fa23
aitajghrmn <businessqehremanqizi@gmail.com>
2026-06-25 12:40:54 +0400

    taskk

---
+++ Tekrar/june/args_kwargs/task4.py
@@ -0,0 +1,6 @@
+def baha_mehsullar(**kwargs):
+    for acar , deyer in kwargs.items():
+        if deyer > 10 :
+            print(f"{acar} bahadir ")
+
+baha_mehsullar(corek=1 , et=19 , sud=3 , kofe =15)
