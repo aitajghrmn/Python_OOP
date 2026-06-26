50c5efae85604d090944b8004438d90e750af5c0
Aitaj <businessqehremanqizi@gmail.com>
2026-06-27 00:46:23 +0400

    task7

---
+++ Tekrar/june/ds/task7.py
@@ -0,0 +1,10 @@
+istifadeciler = [
+    {"ad": "Aytac", "email": "aytac@gmail.com"},
+    {"ad": "Emin", "email": "emin@yahoo.com"},
+    {"ad": "Leyla", "email": "leyla@mail.ru"}
+]
+
+
+emailler = [x["email"] for x in istifadeciler]
+
+print(emailler)
