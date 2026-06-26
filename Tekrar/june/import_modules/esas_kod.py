f79b1649842b64a0b80f9b2ccfadfb3b1096cd4a
Aitaj <businessqehremanqizi@gmail.com>
2026-06-26 22:58:49 +0400

    import modules

---
+++ Tekrar/june/import_modules/esas_kod.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+from hesablamalar import faiz_hesabla
+
+netice = faiz_hesabla(500, 10)
+
+print(f"500 manatın 10%-i: {netice} AZN-dir.")
---
+++ Tekrar/june/import_modules/hesablamalar.py
@@ -0,0 +1,2 @@
+def faiz_hesabla(mebleg, faiz_derecesi):
+    return (mebleg * faiz_derecesi) / 100
