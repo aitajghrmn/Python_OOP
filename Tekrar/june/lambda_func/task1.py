28e3f7fa0f9678d51a50c1a7bfe5a0ace4943bda
Aitaj <businessqehremanqizi@gmail.com>
2026-06-26 22:07:41 +0400

    tas1 for lambda func

---
+++ Tekrar/june/lambda_func/task1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def artir_normal(x):
+    return x + 10
+
+
+artir_lambda = lambda x: x + 10
+
+
+print("Normal funksiya:", artir_normal(5))   
+print("Lambda funksiyası:", artir_lambda(5)) 
