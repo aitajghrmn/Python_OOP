1cdd6dc77162953991cf15a7bf3cce03883b289c
Aitaj <businessqehremanqizi@gmail.com>
2026-06-26 15:55:02 +0400

    taskk 3 for get()

--- Tekrar/june/args_kwargs/task13.py
+++ Tekrar/june/args_kwargs/task13.py
@@ -1,7 +1,10 @@
 def riyaziyyat_bali(**kwargs):
+    # Əgər riyaziyyat yoxdursa, standart olaraq 0 qaytarsın:
+    p = kwargs.get("riyaziyyat", 0)
+    print(f"Riyaziyyat balı: {p}")
 
-    p=kwargs.get("riyaziyyat")
-
-    print(f"Riyaziyyat bali : {p}")
-
+# Test 1: Riyaziyyat var (85 çıxmalıdır)
 riyaziyyat_bali(fizika=90, riyaziyyat=85, kimya=70)
+
+# Test 2: Riyaziyyat YOXDUR (Baxaq None çıxacaq, yoxsa 0?)
+riyaziyyat_bali(fizika=90, kimya=70)
