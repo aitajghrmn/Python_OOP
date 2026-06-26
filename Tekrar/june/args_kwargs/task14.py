bbf83b868eed25ce4c1eb6bebda74a2988a4bd63
Aitaj <businessqehremanqizi@gmail.com>
2026-06-26 16:06:16 +0400

    task4 for get()

---
+++ Tekrar/june/args_kwargs/task14.py
@@ -0,0 +1,8 @@
+def istifadeci_dili(**kwargs):
+
+    d=kwargs.get("dil" , "az")
+    print(f"Istifadeci dili : {d}")
+
+
+istifadeci_dili(ad="Ali", seher="Baku") 
+istifadeci_dili(ad="John", dil="İngilis dili") 
