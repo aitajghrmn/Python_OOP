993b2a1725ba211a538c90d9e2920759ec07b7eb
Aitaj <businessqehremanqizi@gmail.com>
2026-06-30 10:14:33 +0400

    task6

---
+++ Tekrar/june/exceptions/task6.py
@@ -0,0 +1,10 @@
+umumi_hesab = 250
+insan_sayi = 0
+ 
+
+try :
+    adam_basina = umumi_hesab / insan_sayi
+
+except ZeroDivisionError :
+    print("Xəta : Sıfıra bölmə əməliyyatı aparmaq olmaz !")
+    
